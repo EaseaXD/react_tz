197ed0398eb977af977d11b97219303062dcd231
EaseaXD <79414774+EaseaXD@users.noreply.github.com>
2022-01-26 18:59:00 +0200

    add style

--- src/store/profile/index.js
+++ src/store/profile/index.js
@@ -1,10 +1,2 @@
-import Profile from "../../components/Profile/Profile";
 
-export const loginActionTypes ={
-    SET_PROFILE: 'Profile.SET_PROFILE',
-    
-}
 
-export const loginActions = {
-    setProfile: () => ({type: loginActionTypes.SET_PROFILE})
-}
